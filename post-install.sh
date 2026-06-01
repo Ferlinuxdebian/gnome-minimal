1fb5e600bf39bcaec2a775b3da2c269a2b0ace49
Ferlinuxdebian <34031083+Ferlinuxdebian@users.noreply.github.com>
2026-06-01 11:10:50 -0300

    Add command to install Bazaar from Flathub

--- post-install.sh
+++ post-install.sh
@@ -6,3 +6,5 @@ flatpak remote-delete fedora
 sleep 2
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
+
+flatpak install flathub io.github.kolunmi.Bazaar -y
